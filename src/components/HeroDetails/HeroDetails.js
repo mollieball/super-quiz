20e04d68c08cb3cb96bfb09d7aa13aa40372272c
Andrea de Roeck <deroeck.andrea@gmail.com>
2020-03-05 17:14:41 +0000

    Add Score page and fixed hero details component

--- src/components/HeroDetails/HeroDetails.js
+++ src/components/HeroDetails/HeroDetails.js
@@ -1,15 +1,17 @@
 import React from "react";
+import Image from "react-bootstrap/Image";
 import "./HeroDetails.css";
 
-function HeroDetails({ name, image, isCrossed }) {
+function HeroDetails({ name, image, isCrossed, imageStyle }) {
   return (
     <div className="HeroDetails">
       <h1 classname="super-hero-name">{name}</h1>
-      <img
+      <Image
         src={image}
         alt={name}
         className="superhero-profile"
-        style={{ opacity: isCrossed ? 0.5 : 1 }}
+        style={{ opacity: isCrossed ? 0.5 : 1, ...imageStyle }}
+        fluid
       />
     </div>
   );
