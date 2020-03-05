9fa99c5b2475d524f8b9a93f46adf0be17095624
Vicky-Leandra Teixeira <Vickyleandra@users.noreply.github.com>
2020-03-05 14:45:48 +0000

    committing app changes

--- src/App.js
+++ src/App.js
@@ -3,15 +3,17 @@ import { Switch, Route } from "react-router-dom";
 import Home from "./Pages/Home/Home";
 import Play from "./Pages/Play/Play";
 import Score from "./Pages/Score/Score";
+import Timer from "./components/Timer/Timer";
 
 function App() {
   return (
     <React.Fragment>
-      <Switch>
+      <Timer remainingSeconds={0} />
+      {/* <Switch>
         <Route exact path="/" component={Home} />
         <Route path="/play" component={Play} />
         <Route path="/score" component={Score} />
-      </Switch>
+      </Switch> */}
     </React.Fragment>
   );
 }
