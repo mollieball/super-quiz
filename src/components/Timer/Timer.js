c847eb634980d5c4ed8bf0ebbcfa3a004ebbd9c4
Mollie Ball <mollieball@squarepeg.ie>
2020-03-06 12:34:50 +0000

    Update Play page and add music

--- src/components/Timer/Timer.js
+++ src/components/Timer/Timer.js
@@ -1,8 +1,14 @@
 import React from "react";
 import "./Timer.css";
 
-function Timer({ remainingSeconds }) {
-  return <h3>{remainingSeconds}</h3>;
+function Timer({ className, remainingSeconds, ...props }) {
+  return (
+    <div className={`Timer ${className} p-4`}>
+      <h3 className="animated infinite jackInTheBox text-white" {...props}>
+        {remainingSeconds}
+      </h3>
+    </div>
+  );
 }
 
 export default Timer;
